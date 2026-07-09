3f19b7f34461665d152f7a4c95b1985cfb5f5d7a
Muhammad Halim <74248915+HLime-dev@users.noreply.github.com>
2026-07-09 13:26:38 +0800

    Update auto-setup.py

--- auto-setup.py
+++ auto-setup.py
@@ -1,41 +1,8 @@
-#!/system/bin/sh
+#!/data/data/com.termux/files/usr/bin/bash
 
-# Tunggu 30 detik setelah reboot
 sleep 30
 
-# Jalankan script Python
-su -c '
-export PATH=$PATH:/data/data/com.termux/files/usr/bin
-export TERM=xterm-256color
-cd /sdcard/Download
-python obf-wuyx_rejoin.py &
-'
+su -c "export PATH=\$PATH:/data/data/com.termux/files/usr/bin && export TERM=xterm-256color && cd /sdcard/Download && python obf-wuyx_rejoin.py" <<EOF
+1
 
-# Koordinat tap (dibulatkan karena input tap menggunakan integer)
-X1=1194
-Y1=127
-
-X2=251
-Y2=451
-
-X3=596
-Y3=468
-
-X4=1038
-Y4=477
-
-# Loop selamanya
-while true
-do
-    su -c "input tap $X1 $Y1"
-    sleep 60
-
-    su -c "input tap $X2 $Y2"
-    sleep 60
-
-    su -c "input tap $X3 $Y3"
-    sleep 60
-
-    su -c "input tap $X4 $Y4"
-    sleep 60
-done
+EOF
